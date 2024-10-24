e70b0930eacf3f9496d94568c822cc87eea90eda
Tomato0O <183370587+Tomato0O@users.noreply.github.com>
2024-10-24 16:50:38 +0800

    Add component <PageHeader>

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <el-carousel>
+        <el-carousel-item v-for="image in indexImages">
+            <img :src="image" />
+        </el-carousel-item>
+
+    </el-carousel>
+</template>
+
+<style></style>
+
+<script>
+export default {
+    data() {
+        return {
+            indexImages: [
+                require("@/assets/logo.png"),
+                require("@/assets/logo.png"),
+
+            ]
+        }
+    }
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+
+// elementplus组件库：https://element-plus.org/zh-CN/
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+const app = createApp(App);
+
+app.use(router)
+app.use(ElementPlus);
+
+app.mount("#app");
